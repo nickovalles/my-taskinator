293c2e72254868dcef3dd308a815138ce5c19c14
Nilda <nick.ovalles@gmail.com>
2020-11-19 01:03:15 -0700

    <added ability to upfate and delete tasks>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+
 var taskFormHandler = function(event) {
 
   event.preventDefault();
@@ -17,14 +20,39 @@ if (!taskNameInput || !taskTypeInput) {
 
 formEl.reset();
 
-  // package up data as an object
+var isEdit = formEl.hasAttribute("data-task-id");
+
+var completeEditTask = function(taskName, taskType, taskId) {
+// find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
+
+};
+
+// PUT THIS BELOW `var isEdit = ...` in `taskFormHandler()`
+
+// has data attribute, so get task id and call function to complete edit process
+if (isEdit) {
+  var taskId = formEl.getAttribute("data-task-id");
+  completeEditTask(taskNameInput, taskTypeInput, taskId);
+} 
+// no data attribute, so create object as normal and pass to createTaskEl function
+else {
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
   };
 
-  // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+}
 }
 
 
@@ -141,9 +169,20 @@ var taskButtonHandler = function(event) {
       deleteTask(taskId);
   }
 };
+
+var taskStatusChangeHandler = function(event) {
+  // get the task item's id
+  var taskId = event.target.getAttribute("data-task-id");
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase();
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+};
   
 
-
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
 
 
   // Main class element event listener
