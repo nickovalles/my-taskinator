5703dc428c7862cc4ce5f0a7a97ee491b25a47a6
Nilda <nick.ovalles@gmail.com>
2020-11-18 17:55:53 -0700

    <task handler function to the form>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", createTaskHandler);
-  var listItemEl = document.createElement("li");
+
+var createTaskHandler = function(event) {
+  event.preventDefault();
+
+  var listItemE1 = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  listItemEl.textContent = "This is a new task";
   tasksToDoEl.appendChild(listItemEl);
+};
 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
 
-  
+
+
+
+
+
+
+
+  // Event Listener for the form
+  formEl.addEventListener("submit", createTaskHandler);
